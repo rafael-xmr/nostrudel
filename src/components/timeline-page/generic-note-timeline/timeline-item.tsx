636453ce5e02882de43dfe51594965bd3b9bc396
Rafael Saes <git@rafael.saes.dev>
2024-12-01 15:24:36 -0300

    chore: misc

--- src/components/timeline-page/generic-note-timeline/timeline-item.tsx
+++ src/components/timeline-page/generic-note-timeline/timeline-item.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, memo } from "react";
+import { type ReactNode, memo, lazy } from "react";
 import { kinds } from "nostr-tools";
 import { Box, Text } from "@chakra-ui/react";
 
@@ -10,48 +10,59 @@ import RelayRecommendation from "./relay-recommendation";
 import BadgeAwardCard from "../../../views/badges/components/badge-award-card";
 import { isReply } from "../../../helpers/nostr/event";
 import { STREAM_KIND } from "../../../helpers/nostr/stream";
-import { NostrEvent } from "../../../types/nostr-event";
+import type { NostrEvent } from "../../../types/nostr-event";
 import { FLARE_VIDEO_KIND } from "../../../helpers/nostr/video";
-import EmbeddedFlareVideo from "../../embed-event/event-types/embedded-flare-video";
 import { TimelineNote } from "../../note/timeline-note";
 import useEventIntersectionRef from "../../../hooks/use-event-intersection-ref";
 
-function TimelineItem({ event, visible, minHeight }: { event: NostrEvent; visible: boolean; minHeight?: number }) {
-  const ref = useEventIntersectionRef(event);
+const EmbeddedFlareVideo = lazy(
+	() => import("../../embed-event/event-types/embedded-flare-video"),
+);
 
-  let content: ReactNode | null = null;
-  switch (event.kind) {
-    case kinds.ShortTextNote:
-      content = isReply(event) ? <ReplyNote event={event} /> : <TimelineNote event={event} showReplyButton />;
-      break;
-    case kinds.Repost:
-    case kinds.GenericRepost:
-      content = <RepostEvent event={event} />;
-      break;
-    case STREAM_KIND:
-      content = <StreamNote event={event} />;
-      break;
-    case kinds.RecommendRelay:
-      content = <RelayRecommendation event={event} />;
-      break;
-    case kinds.BadgeAward:
-      content = <BadgeAwardCard award={event} />;
-      break;
-    case FLARE_VIDEO_KIND:
-      content = <EmbeddedFlareVideo video={event} />;
-      break;
-    default:
-      content = <Text>Unknown event kind: {event.kind}</Text>;
-      break;
-  }
+function TimelineItem({
+	event,
+	visible,
+	minHeight,
+}: { event: NostrEvent; visible: boolean; minHeight?: number }) {
+	const ref = useEventIntersectionRef(event);
 
-  return (
-    <ErrorBoundary event={event}>
-      <Box minHeight={minHeight + "px"} ref={ref}>
-        {visible && content}
-      </Box>
-    </ErrorBoundary>
-  );
+	let content: ReactNode | null = null;
+	switch (event.kind) {
+		case kinds.ShortTextNote:
+			content = isReply(event) ? (
+				<ReplyNote event={event} />
+			) : (
+				<TimelineNote event={event} showReplyButton />
+			);
+			break;
+		case kinds.Repost:
+		case kinds.GenericRepost:
+			content = <RepostEvent event={event} />;
+			break;
+		case STREAM_KIND:
+			content = <StreamNote event={event} />;
+			break;
+		case kinds.RecommendRelay:
+			content = <RelayRecommendation event={event} />;
+			break;
+		case kinds.BadgeAward:
+			content = <BadgeAwardCard award={event} />;
+			break;
+		case FLARE_VIDEO_KIND:
+			content = <EmbeddedFlareVideo video={event} />;
+			break;
+		default:
+			content = <Text>Unknown event kind: {event.kind}</Text>;
+			break;
+	}
+
+	return (
+		<ErrorBoundary event={event}>
+			<Box minHeight={minHeight + "px"} ref={ref}>
+				{visible && content}
+			</Box>
+		</ErrorBoundary>
+	);
 }
 
 export default memo(TimelineItem);
--- src/views/relays/app/index.tsx
+++ src/views/relays/app/index.tsx
@@ -1,5 +1,15 @@
 import { MouseEventHandler, useCallback, useMemo } from "react";
-import { Button, Card, CardBody, CardHeader, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+	Button,
+	Card,
+	CardBody,
+	CardHeader,
+	Flex,
+	Heading,
+	SimpleGrid,
+	Text,
+	Link,
+} from "@chakra-ui/react";
 import { kinds } from "nostr-tools";
 import { WarningIcon } from "@chakra-ui/icons";
 import { useObservable } from "applesauce-react/hooks";
@@ -9,167 +19,230 @@ import WifiOff from "../../../components/icons/wifi-off";
 import Wifi from "../../../components/icons/wifi";
 import BackButton from "../../../components/router/back-button";
 import AddRelayForm from "./add-relay-form";
-import clientRelaysService, { recommendedReadRelays, recommendedWriteRelays } from "../../../services/client-relays";
+import clientRelaysService, {
+	KNOWN_MONERO_RELAYS,
+	KNOWN_ONION_RELAYS,
+	recommendedReadRelays,
+	recommendedWriteRelays,
+} from "../../../services/client-relays";
 import { RelayMode } from "../../../classes/relay";
 import RelaySet from "../../../classes/relay-set";
-import { useReadRelays, useWriteRelays } from "../../../hooks/use-client-relays";
+import {
+	useReadRelays,
+	useWriteRelays,
+} from "../../../hooks/use-client-relays";
 import useCurrentAccount from "../../../hooks/use-current-account";
 import RelayControl from "./relay-control";
 import { getRelaysFromExt } from "../../../helpers/nip07";
 import { useUserDNSIdentity } from "../../../hooks/use-user-dns-identity";
 import useUserContactRelays from "../../../hooks/use-user-contact-relays";
 import SelectRelaySet from "./select-relay-set";
-import { safeRelayUrls } from "../../../helpers/relay";
 import HoverLinkOverlay from "../../../components/hover-link-overlay";
 import useReplaceableEvent from "../../../hooks/use-replaceable-event";
 
-const JapaneseRelays = safeRelayUrls([
-  "wss://r.kojira.io",
-  "wss://nrelay-jp.c-stellar.net",
-  "wss://nostr.fediverse.jp",
-  "wss://nostr.holybea.com",
-  "wss://relay-jp.nostr.wirednet.jp",
-]);
+function RelaySetCard({
+	label,
+	read,
+	write,
+	desc,
+}: {
+	label: string;
+	read: Iterable<string>;
+	write: Iterable<string>;
+	desc?: JSX.Element | string;
+}) {
+	const handleClick = useCallback<MouseEventHandler>((e) => {
+		e.preventDefault();
+		clientRelaysService.readRelays.next(RelaySet.from(read));
+		clientRelaysService.writeRelays.next(RelaySet.from(write));
+		clientRelaysService.saveRelays();
+	}, []);
 
-function RelaySetCard({ label, read, write }: { label: string; read: Iterable<string>; write: Iterable<string> }) {
-  const handleClick = useCallback<MouseEventHandler>((e) => {
-    e.preventDefault();
-    clientRelaysService.readRelays.next(RelaySet.from(read));
-    clientRelaysService.writeRelays.next(RelaySet.from(write));
-    clientRelaysService.saveRelays();
-  }, []);
-
-  return (
-    <Card w="full" variant="outline">
-      <CardHeader px="4" pt="4" pb="2">
-        <Heading size="sm">
-          <HoverLinkOverlay href="#" onClick={handleClick}>
-            {label}:
-          </HoverLinkOverlay>
-        </Heading>
-      </CardHeader>
-      <CardBody px="4" pt="0" pb="4">
-        {RelaySet.from(read, write).urls.map((url) => (
-          <Text key={url} whiteSpace="pre" isTruncated>
-            {url}
-          </Text>
-        ))}
-      </CardBody>
-    </Card>
-  );
+	return (
+		<>
+			<Heading size="sm">{label}:</Heading>
+			{desc ? <Text fontStyle="italic">{desc}</Text> : <></>}
+			<Card w="full" variant="outline">
+				<CardHeader px="4" pt="4" pb="2">
+					<Heading size="sm">
+						<HoverLinkOverlay href="#" onClick={handleClick} />
+					</Heading>
+				</CardHeader>
+				<CardBody px="4" pt="0" pb="4">
+					{RelaySet.from(read, write).urls.map((url) => (
+						<Text key={url} whiteSpace="pre" isTruncated>
+							{url}
+						</Text>
+					))}
+				</CardBody>
+			</Card>
+		</>
+	);
 }
 
 export default function AppRelays() {
-  const account = useCurrentAccount();
-  const readRelays = useReadRelays();
-  const writeRelays = useWriteRelays();
-  const offline = useObservable(offlineMode);
-  const nip65 = useReplaceableEvent(account?.pubkey ? { kind: kinds.RelayList, pubkey: account?.pubkey } : undefined);
-  const nip05 = useUserDNSIdentity(account?.pubkey);
-  const contactRelays = useUserContactRelays(account?.pubkey);
+	const account = useCurrentAccount();
+	const readRelays = useReadRelays();
+	const writeRelays = useWriteRelays();
+	const offline = useObservable(offlineMode);
+	const nip65 = useReplaceableEvent(
+		account?.pubkey
+			? { kind: kinds.RelayList, pubkey: account?.pubkey }
+			: undefined,
+	);
+	const nip05 = useUserDNSIdentity(account?.pubkey);
+	const contactRelays = useUserContactRelays(account?.pubkey);
 
-  const sorted = useMemo(() => RelaySet.from(readRelays, writeRelays).urls.sort(), [readRelays, writeRelays]);
+	const sorted = useMemo(
+		() => RelaySet.from(readRelays, writeRelays).urls.sort(),
+		[readRelays, writeRelays],
+	);
 
-  return (
-    <Flex gap="2" direction="column" overflow="auto hidden" flex={1}>
-      <Flex gap="2" alignItems="center">
-        <BackButton hideFrom="lg" size="sm" />
-        <Heading size="lg" px={{ base: 0, lg: "2" }}>
-          App Relays
-        </Heading>
-        <Button
-          onClick={() => offlineMode.next(!offline)}
-          leftIcon={offline ? <WifiOff /> : <Wifi />}
-          ml="auto"
-          size={{ base: "sm", lg: "md" }}
-        >
-          {offline ? "Offline" : "Online"}
-        </Button>
-      </Flex>
+	return (
+		<Flex gap="2" direction="column" overflow="auto hidden" flex={1}>
+			<Flex gap="2" alignItems="center">
+				<BackButton hideFrom="lg" size="sm" />
+				<Heading size="lg" px={{ base: 0, lg: "2" }}>
+					App Relays
+				</Heading>
+				<Button
+					onClick={() => offlineMode.next(!offline)}
+					leftIcon={offline ? <WifiOff /> : <Wifi />}
+					ml="auto"
+					size={{ base: "sm", lg: "md" }}
+				>
+					{offline ? "Offline" : "Online"}
+				</Button>
+			</Flex>
 
-      <Text fontStyle="italic" px="2" mt="-2">
-        These relays are stored locally and are used for everything in the app
-      </Text>
+			<Text fontStyle="italic" px="2" mt="-2">
+				These relays are stored locally and are used for everything in the app
+			</Text>
 
-      {sorted.map((url) => (
-        <RelayControl key={url} url={url} />
-      ))}
-      <AddRelayForm
-        onSubmit={(url) => {
-          clientRelaysService.addRelay(url, RelayMode.ALL);
-        }}
-      />
+			{sorted.map((url) => (
+				<RelayControl key={url} url={url} />
+			))}
+			<AddRelayForm
+				onSubmit={(url) => {
+					clientRelaysService.addRelay(url, RelayMode.ALL);
+				}}
+			/>
 
-      {writeRelays.size === 0 && (
-        <Text color="yellow.500">
-          <WarningIcon /> There are no write relays set, any note you create might not be saved
-        </Text>
-      )}
+			{writeRelays.size === 0 && (
+				<Text color="yellow.500">
+					<WarningIcon /> There are no write relays set, any note you create
+					might not be saved
+				</Text>
+			)}
 
-      <Heading size="md" mt="2">
-        Set from:
-      </Heading>
-      <Flex wrap="wrap" gap="2">
-        {window.nostr && (
-          <Button
-            onClick={async () => {
-              const { read, write } = await getRelaysFromExt();
-              clientRelaysService.readRelays.next(read);
-              clientRelaysService.writeRelays.next(write);
-              clientRelaysService.saveRelays();
-            }}
-          >
-            Extension
-          </Button>
-        )}
-        {nip65 && (
-          <Button
-            onClick={() => {
-              clientRelaysService.setRelaysFromRelaySet(nip65);
-            }}
-          >
-            NIP-65 (Mailboxes)
-          </Button>
-        )}
-        {nip05 && (
-          <Button
-            onClick={() => {
-              clientRelaysService.readRelays.next(RelaySet.from(nip05.relays));
-              clientRelaysService.writeRelays.next(RelaySet.from(nip05.relays));
-              clientRelaysService.saveRelays();
-            }}
-          >
-            NIP-05
-          </Button>
-        )}
-        {contactRelays && (
-          <Button
-            onClick={() => {
-              clientRelaysService.readRelays.next(contactRelays.inbox);
-              clientRelaysService.writeRelays.next(contactRelays.outbox);
-              clientRelaysService.saveRelays();
-            }}
-          >
-            Contact List (Legacy)
-          </Button>
-        )}
-      </Flex>
-      {account && (
-        <>
-          <Heading size="md" mt="2">
-            Use relay set
-          </Heading>
-          <SelectRelaySet onChange={(cord, set) => set && clientRelaysService.setRelaysFromRelaySet(set)} />
-        </>
-      )}
+			<Heading size="md" mt="2">
+				Set from:
+			</Heading>
+			<Flex wrap="wrap" gap="2">
+				{window.nostr && (
+					<Button
+						onClick={async () => {
+							const { read, write } = await getRelaysFromExt();
+							clientRelaysService.readRelays.next(read);
+							clientRelaysService.writeRelays.next(write);
+							clientRelaysService.saveRelays();
+						}}
+					>
+						Extension
+					</Button>
+				)}
+				{nip65 && (
+					<Button
+						onClick={() => {
+							clientRelaysService.setRelaysFromRelaySet(nip65);
+						}}
+					>
+						NIP-65 (Mailboxes)
+					</Button>
+				)}
+				{nip05 && (
+					<Button
+						onClick={() => {
+							clientRelaysService.readRelays.next(RelaySet.from(nip05.relays));
+							clientRelaysService.writeRelays.next(RelaySet.from(nip05.relays));
+							clientRelaysService.saveRelays();
+						}}
+					>
+						NIP-05
+					</Button>
+				)}
+				{contactRelays && (
+					<Button
+						onClick={() => {
+							clientRelaysService.readRelays.next(contactRelays.inbox);
+							clientRelaysService.writeRelays.next(contactRelays.outbox);
+							clientRelaysService.saveRelays();
+						}}
+					>
+						Contact List (Legacy)
+					</Button>
+				)}
+			</Flex>
+			{account && (
+				<>
+					<Heading size="md" mt="2">
+						Use relay set
+					</Heading>
+					<SelectRelaySet
+						onChange={(cord, set) =>
+							set && clientRelaysService.setRelaysFromRelaySet(set)
+						}
+					/>
+				</>
+			)}
 
-      <Heading size="md" mt="2">
-        Presets:
-      </Heading>
-      <SimpleGrid columns={{ base: 1, lg: 2, xl: 3 }} spacing="2">
-        <RelaySetCard label="Popular Relays" read={recommendedReadRelays} write={recommendedWriteRelays} />
-        <RelaySetCard label="Japanese relays" read={JapaneseRelays} write={JapaneseRelays} />
-      </SimpleGrid>
-    </Flex>
-  );
+			<Heading size="md" mt="2">
+				Presets:
+			</Heading>
+			<SimpleGrid columns={{ base: 1, lg: 2, xl: 3 }} spacing="2">
+				<RelaySetCard
+					label="Popular Relays"
+					read={recommendedReadRelays}
+					write={recommendedWriteRelays}
+					desc="Mainstream relays, find most normie activity (and spam) here."
+				/>
+				<RelaySetCard
+					label="Monero Relays"
+					read={KNOWN_MONERO_RELAYS}
+					write={KNOWN_MONERO_RELAYS}
+					desc={
+						<>
+							Find them at{" "}
+							<Link
+								href="https://github.com/hundehausen/monero-nostr-relays"
+								isExternal
+								color="blue.500"
+							>
+								https://github.com/hundehausen/monero-nostr-relays
+							</Link>{" "}
+							These relays are whitelisted or require a Monero payment to use.
+							(so less spam)
+						</>
+					}
+				/>
+				<RelaySetCard
+					label="Onion Relays"
+					read={KNOWN_ONION_RELAYS}
+					write={KNOWN_ONION_RELAYS}
+					desc={
+						<>
+							Find them at{" "}
+							<Link
+								href="https://github.com/0xtrr/onion-service-nostr-relays"
+								isExternal
+								color="blue.500"
+							>
+								https://github.com/0xtrr/onion-service-nostr-relays
+							</Link>
+						</>
+					}
+				/>
+			</SimpleGrid>
+		</Flex>
+	);
 }
