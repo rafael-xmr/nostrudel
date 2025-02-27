3a4f5c9f191eedbca31a447de8aad5781efcb510
Rafael Saes <git@rafael.saes.dev>
2025-02-27 12:04:50 -0300

    feat: support button color theme

--- src/components/support-button.tsx
+++ src/components/support-button.tsx
@@ -13,9 +13,11 @@ import {
 	TabPanel,
 	Spinner,
 	Flex,
+	useTheme,
+	useColorModeValue,
 } from "@chakra-ui/react";
 import { ZapModalContents, ZapModalHeader } from "./event-zap-modal";
-import { MoneroBlackIcon } from "./icons";
+import { MoneroBlackIcon, MoneroWhiteIcon } from "./icons";
 import { useBreakpointValue } from "~/providers/global/breakpoint-provider";
 import { useEffect, useRef, useState } from "react";
 import { useAsync } from "react-use";
@@ -79,18 +81,23 @@ export default function SupportButton() {
 		}
 	}, [account, tabIndex]);
 
+	const theme = useTheme();
+	const token = theme.semanticTokens.colors["chakra-body-text"];
+	const color = useColorModeValue(token._light, token._dark) as string;
+	const MoneroIcon = color !== token._light ? MoneroBlackIcon : MoneroWhiteIcon;
+
 	return (
 		<>
 			<Button
 				aria-label={label}
 				title={label}
-				leftIcon={<MoneroBlackIcon boxSize={5} />}
+				leftIcon={<MoneroIcon boxSize={5} />}
 				variant="solid"
 				p="2"
 				justifyContent="flex-start"
 				colorScheme="primary"
 				flexShrink={0}
-				backgroundColor="white"
+				backgroundColor={color}
 				onClick={onOpen}
 			>
 				{label}
