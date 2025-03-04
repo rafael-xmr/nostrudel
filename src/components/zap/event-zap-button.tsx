6199f5322d1ab61bff9368ea0aa020e2fe12369b
Rafael Saes <git@rafael.saes.dev>
2025-03-04 07:36:44 -0300

    feat: no address tip message in modal, add github link in sidebar

--- src/components/zap/event-zap-button.tsx
+++ src/components/zap/event-zap-button.tsx
@@ -38,7 +38,7 @@ export default function EventZapButton({
 				title={title}
 				{...props}
 				onClick={onOpen}
-				isDisabled={!canZap}
+				opacity={!canZap ? "0.4" : "1"}
 			/>
 
 			{isOpen && (
